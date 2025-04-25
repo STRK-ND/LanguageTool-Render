bafbef169e1bf139e22c895f04a95735fdff75d6
Rajat Kashyap <50959267+STRK-ND@users.noreply.github.com>
2025-04-25 11:23:46 +0530

    Update languagetool.sh

--- languagetool.sh
+++ languagetool.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-#(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /ngrams-download/ && cd /ngrams-tmp/ && jar -xvf /ngrams-download/ngrams-en-20150817.zip && mv /ngrams-tmp/* /ngrams/ && rm /ngrams-download/ngrams-en-20150817.zip && echo Ngram has been set up) &
-java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 10000 --public --allow-origin '*' #--languageModel /ngrams/
+set -e
+
+# Start the LanguageTool server
+JAVA_OPTS="-Xms256m -Xmx512m"
+SERVER_JAR="languagetool-server.jar"
+
+exec java $JAVA_OPTS -cp "$SERVER_JAR" org.languagetool.server.HTTPServer --port "$PORT" --public --allow-origin '*'
